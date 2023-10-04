3b603161e200851b62bff1a9e64e8c7db5ef3a58
Shantik998 <shantik998@gmail.com>
2023-10-03 19:30:29 -0500

    updated file

--- v1_emit_message.py
+++ v1_emit_message.py
@@ -9,7 +9,7 @@ import pika
 message = ['Hello World!','Hello Shanti','Hello Dr Case']
 
 # create a blocking connection to the RabbitMQ server
-conn = pika.BlockingConnection(pika.ConnectionParameters("LOCALHOST"))
+conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 
 # use the connection to create a communication channel
 ch = conn.channel()
